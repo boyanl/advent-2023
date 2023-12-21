d0b8380515a1661749683b76b6a5dbb5eaca797b
Boyan Lazov <boyan.lazov@gmail.com>
2023-12-21 19:26:02 +0200

    Add a more geometric solution (stolen ofc) and unify part 1/2 code a bit

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -5,8 +5,8 @@ use std::{
 
 #[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
 struct Pos {
-    x: usize,
-    y: usize,
+    x: i32,
+    y: i32,
 }
 
 fn read_input() -> (Vec<Vec<char>>, Pos) {
@@ -16,7 +16,10 @@ fn read_input() -> (Vec<Vec<char>>, Pos) {
         map.push(line.chars().collect());
 
         if let Some(j) = line.find("S") {
-            start = Pos { x: j, y: i };
+            start = Pos {
+                x: j as i32,
+                y: i as i32,
+            };
         }
     }
 
@@ -28,8 +31,13 @@ fn neighbours(p: Pos, map: &Vec<Vec<char>>) -> Vec<Pos> {
     let mut result = Vec::new();
 
     for (dx, dy) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
-        let (nx, ny) = ((p.x as i32 + dx) as usize, (p.y as i32 + dy) as usize);
-        if nx < m && ny < n && map[ny][nx] == '.' {
+        let (nx, ny) = (p.x + dx, p.y + dy);
+        if nx >= 0
+            && nx < m as i32
+            && ny >= 0
+            && ny < n as i32
+            && "S.".contains(map[ny as usize][nx as usize])
+        {
             result.push(Pos { x: nx, y: ny });
         }
     }
@@ -37,22 +45,7 @@ fn neighbours(p: Pos, map: &Vec<Vec<char>>) -> Vec<Pos> {
     result
 }
 
-fn show_reachable(ps: &HashSet<Pos>, map: &Vec<Vec<char>>) {
-    for i in 0..map.len() {
-        for j in 0..map[i].len() {
-            if map[i][j] == 'S' {
-                print!("S");
-            } else if ps.contains(&Pos { x: j, y: i }) {
-                print!("O");
-            } else {
-                print!("{}", map[i][j]);
-            }
-        }
-        println!();
-    }
-}
-
-fn reachable_count(start: Pos, d: i32, map: &Vec<Vec<char>>) -> (i32, i32) {
+fn reachable_count(start: Pos, d: i32, map: &Vec<Vec<char>>) -> i32 {
     let mut q = VecDeque::new();
     q.push_back((start, 0));
 
@@ -60,15 +53,11 @@ fn reachable_count(start: Pos, d: i32, map: &Vec<Vec<char>>) -> (i32, i32) {
     visited.insert(start);
 
     let mut res = 0;
-    let mut max_dist = -1;
 
     while !q.is_empty() {
         let (pos, dist) = q.pop_front().unwrap();
         if dist % 2 == d % 2 {
             res += 1;
-            if dist > max_dist {
-                max_dist = dist;
-            }
         }
 
         for n in neighbours(pos, map) {
@@ -79,24 +68,18 @@ fn reachable_count(start: Pos, d: i32, map: &Vec<Vec<char>>) -> (i32, i32) {
         }
     }
 
-    (res, max_dist)
+    res
 }
 
 fn part_one() {
     let (map, start) = read_input();
     let steps = 64;
-    let (result, _) = reachable_count(start, steps, &map);
+    let result = reachable_count(start, steps, &map);
 
     println!("{result}");
 }
 
-#[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
-struct Pos2 {
-    x: i32,
-    y: i32,
-}
-
-fn neighbours_2(p: Pos2, map: &Vec<Vec<char>>) -> Vec<Pos2> {
+fn neighbours_2(p: Pos, map: &Vec<Vec<char>>) -> Vec<Pos> {
     let (n, m) = (map.len(), map[0].len());
     let mut result = Vec::new();
 
@@ -105,27 +88,25 @@ fn neighbours_2(p: Pos2, map: &Vec<Vec<char>>) -> Vec<Pos2> {
         let (mx, my) = (nx.rem_euclid(m as i32), ny.rem_euclid(n as i32));
         let c = map[my as usize][mx as usize];
         if c == '.' || c == 'S' {
-            result.push(Pos2 { x: nx, y: ny });
+            result.push(Pos { x: nx, y: ny });
         }
     }
 
     result
 }
 
-fn reachable_count_2_naive(start: Pos2, d: i32, map: &Vec<Vec<char>>) -> i32 {
+fn reachable_cells_naive(start: Pos, d: i32, map: &Vec<Vec<char>>) -> HashSet<Pos> {
     let mut q = VecDeque::new();
     q.push_back((start, 0));
 
     let mut visited = HashSet::new();
     visited.insert(start);
 
-    let mut res = 0;
     let mut cells = HashSet::new();
 
     while !q.is_empty() {
         let (pos, dist) = q.pop_front().unwrap();
         if dist % 2 == d % 2 {
-            res += 1;
             cells.insert(pos);
         }
 
@@ -137,10 +118,41 @@ fn reachable_count_2_naive(start: Pos2, d: i32, map: &Vec<Vec<char>>) -> i32 {
         }
     }
 
-    res
+    cells
 }
 
-fn reachable_count_2_smart(start: Pos2, max_dist: i32, map: &Vec<Vec<char>>) -> i64 {
+fn visualize_reachable_cells(cells: &HashSet<Pos>, map: &Vec<Vec<char>>) {
+    let (h, w) = (map.len() as i32, map[0].len() as i32);
+
+    let min_x = cells.iter().map(|p| p.x).min().unwrap();
+    let min_y = cells.iter().map(|p| p.y).min().unwrap();
+    let max_x = cells.iter().map(|p| p.x).max().unwrap();
+    let max_y = cells.iter().map(|p| p.y).max().unwrap();
+
+    for y in min_y - 1..=max_y + 1 {
+        if y % h == 0 {
+            println!();
+        }
+        for x in min_x - 1..=max_x + 1 {
+            if x % w == 0 {
+                print!(" ");
+            }
+            let pos = Pos { x, y };
+            let (mx, my) = (x.rem_euclid(w) as usize, y.rem_euclid(h) as usize);
+            if map[my][mx] == 'S' && x >= 0 && x < w && y >= 0 && y < h {
+                print!("S");
+            } else if cells.contains(&pos) {
+                print!("O");
+            } else {
+                let c = if map[my][mx] == '#' { '#' } else { '.' };
+                print!("{}", c);
+            }
+        }
+        println!();
+    }
+}
+
+fn reachable_count_2_recurrence_relation(start: Pos, max_dist: i32, map: &Vec<Vec<char>>) -> i64 {
     let mut q = HashSet::new();
     q.insert(start);
 
@@ -200,15 +212,50 @@ fn reachable_count_2_smart(start: Pos2, max_dist: i32, map: &Vec<Vec<char>>) -> 
     count
 }
 
+fn reachable_count_2_fill(start: Pos, max_dist: i32, map: &Vec<Vec<char>>) -> i64 {
+    let (h, w) = (map.len() as i32, map[0].len() as i32);
+    assert!(h == w);
+    let n = h;
+
+    assert!(max_dist % n == n / 2);
+    assert!(start.x == n / 2);
+    assert!(start.y == n / 2);
+    let odd = reachable_count(start, 2 * n + 1, map) as i64;
+    let even = reachable_count(start, 2 * n, map) as i64;
+
+    let grid_half_diag = ((max_dist - (n / 2)) / n) as i64;
+    let odd_cnt = ((grid_half_diag / 2) * 2 - 1).pow(2);
+    let even_cnt = ((grid_half_diag / 2) * 2).pow(2);
+
+    let corner_top = reachable_count(Pos { x: n / 2, y: n - 1 }, n - 1, map) as i64;
+    let corner_right = reachable_count(Pos { x: 0, y: n / 2 }, n - 1, map) as i64;
+    let corner_bottom = reachable_count(Pos { x: n / 2, y: 0 }, n - 1, map) as i64;
+    let corner_left = reachable_count(Pos { x: n - 1, y: n / 2 }, n - 1, map) as i64;
+
+    let small_top_right = reachable_count(Pos { x: 0, y: n - 1 }, n / 2 - 1, map) as i64;
+    let small_top_left = reachable_count(Pos { x: n - 1, y: n - 1 }, n / 2 - 1, map) as i64;
+    let small_bottom_left = reachable_count(Pos { x: n - 1, y: 0 }, n / 2 - 1, map) as i64;
+    let small_bottom_right = reachable_count(Pos { x: 0, y: 0 }, n / 2 - 1, map) as i64;
+
+    let large_top_right = reachable_count(Pos { x: 0, y: n - 1 }, n + n / 2 - 1, map) as i64;
+    let large_top_left = reachable_count(Pos { x: n - 1, y: n - 1 }, n + n / 2 - 1, map) as i64;
+    let large_bottom_left = reachable_count(Pos { x: n - 1, y: 0 }, n + n / 2 - 1, map) as i64;
+    let large_bottom_right = reachable_count(Pos { x: 0, y: 0 }, n + n / 2 - 1, map) as i64;
+
+    return odd_cnt * odd
+        + even_cnt * even
+        + (corner_top + corner_right + corner_bottom + corner_left)
+        + grid_half_diag
+            * (small_top_left + small_top_right + small_bottom_left + small_bottom_right)
+        + (grid_half_diag - 1)
+            * (large_top_left + large_top_right + large_bottom_left + large_bottom_right);
+}
+
 fn part_two() {
     let (map, start) = read_input();
 
-    let start = Pos2 {
-        x: start.x as i32,
-        y: start.y as i32,
-    };
     let steps = 26501365;
-    let result = reachable_count_2_smart(start, steps, &map);
+    let result = reachable_count_2_fill(start, steps, &map);
     println!("{result}");
 }
 
